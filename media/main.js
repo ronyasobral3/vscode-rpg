e0e627f542bfd0300d078a05c300a9e696f7575a
rony1533 <ronysobral5@gmail.com>
2025-07-09 20:43:33 -0300

    feat: adicionando logica de dano

--- media/main.js
+++ media/main.js
@@ -6,7 +6,8 @@
     const message = event.data;
     switch (message.command) {
       case 'updateMonsterStyle':
-        updateMonster(message.value);
+        updateMonster();
+        showDamage("-1");
         break;
       case 'changeMonster':
         changeMoster(message.value);
@@ -17,38 +18,62 @@
     }
   });
 
-  function updateMonster(textLength) {
+  function updateMonster() {
     if (monster) {
-      if (textLength > 0) {
-        // Ativa o efeito de balancear e mudar a cor
-        monster.style.animation = 'shake 0.3s ease-in-out';
-        monster.style.filter = 'grayscale(100%) brightness(40%) sepia(100%) hue-rotate(-50deg) saturate(600%) contrast(0.8)';
+      // Ativa o efeito de balancear e mudar a cor
+      monster.style.animation = 'shake 0.3s ease-in-out';
+      monster.style.filter = 'grayscale(100%) brightness(40%) sepia(100%) hue-rotate(-50deg) saturate(600%) contrast(0.8)';
 
-        setTimeout(() => {
-          monster.style.animation = 'none';
-          monster.style.filter = 'none';
-        }, 600);
-      } else {
-        // Remove o efeito imediatamente se não houver texto
-        monster.style.animation = 'none';
+      setTimeout(() => {
+        monster.style.animation = 'pulse 2s infinite alternate';
+        monster.offsetWidth;
         monster.style.filter = 'none';
-      }
+      }, 600); // remove o efeito de dano apos 600 milesegundos
+
     }
   }
 
+
   function changeMoster(src) {
-      if (monster) {
-        monster.src = src;
+    if (monster) {
+      monster.src = src;
     } else {
-        console.error('Elemento de imagem não encontrado');
+      console.error('Elemento de imagem não encontrado');
     }
   }
 
   function updateMonsterLife(lifePoints) {
     let maxLife = 30;
-    let percentLif = (lifePoints / maxLife) *100;
+    let percentLif = (lifePoints / maxLife) * 100;
 
-    healthIndicator.style.width = percentLif+'%';
+    healthIndicator.style.width = percentLif + '%';
+  }
+
+  function showDamage(text) {
+    if (!monster) { return; }
+
+    const damage = document.createElement("div");
+    damage.className = "damage-float";
+    damage.textContent = text;
+
+    // Posiciona próximo ao monstro
+    const rect = monster.getBoundingClientRect();
+
+    damage.style.left = `${getRandomInt(0, 110) + rect.width / 2}px`;
+    damage.style.top = `${rect.top}px`;
+    damage.style.position = "fixed";
+
+    document.body.appendChild(damage);
+
+    setTimeout(() => {
+      damage.remove();
+    }, 1000); // Remove o dano após a animação
+  }
+
+  function getRandomInt(min, max) {
+    var min = Math.ceil(min);
+    var max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
 })();
--- src/extension.ts
+++ src/extension.ts
@@ -1,114 +1,149 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
+  const provider = new rpgViewProvider(context.extensionUri);
 
-	const provider = new rpgViewProvider(context.extensionUri);
-
-	context.subscriptions.push(
-		vscode.window.registerWebviewViewProvider(rpgViewProvider.viewType, provider));
+  context.subscriptions.push(
+    vscode.window.registerWebviewViewProvider(
+      rpgViewProvider.viewType,
+      provider
+    )
+  );
 }
 
 class rpgViewProvider implements vscode.WebviewViewProvider {
+  public static readonly viewType = "rpg";
 
-	public static readonly viewType = 'rpg';
+  private _view?: vscode.WebviewView;
+  private monsterLife: number = 30;
+  private monsterImg: string = "Icon";
+  private range1to10: number = 1;
+  private monsterView: string = "";
 
-	private _view?: vscode.WebviewView;
+  constructor(private readonly _extensionUri: vscode.Uri) {
+    this.monsterView = this.monsterImg + this.range1to10.toString() + ".png";
+  }
 
-	private monsterLife: number = 30;
-	private defaultMonster: string = 'Frog.png';
-	private listMonster: Array<string> = ['Frog.png','slime.png','Ogre.png', 'Mummy.png', 'wizard.png'];
+  public resolveWebviewView(
+    webviewView: vscode.WebviewView,
+    context: vscode.WebviewViewResolveContext,
+    _token: vscode.CancellationToken
+  ) {
+    this._view = webviewView;
 
-	constructor(
-		private readonly _extensionUri: vscode.Uri,
-	) { }
+    webviewView.webview.options = {
+      enableScripts: true,
+      localResourceRoots: [vscode.Uri.joinPath(this._extensionUri, "media")],
+    };
 
-	public resolveWebviewView(
-		webviewView: vscode.WebviewView,
-		context: vscode.WebviewViewResolveContext,
-		_token: vscode.CancellationToken,
-	) {
-		this._view = webviewView;
+    webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
 
-		webviewView.webview.options = {
-			enableScripts: true,
-			localResourceRoots: [
-				vscode.Uri.joinPath(this._extensionUri, 'media')
-			]
-		};
+    // Escuta as alterações feitas no texto
+    vscode.workspace.onDidChangeTextDocument((event) => {
+      this.monsterLife--;
 
-		webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
+      this.updateMonsterStyle();
+      this.updateMonsterLife(this.monsterLife);
 
-		// Escuta as alterações feitas no texto 
-		vscode.window.onDidChangeTextEditorSelection(event => {
-			this.monsterLife--;
-			this.updateMonsterStyle(event.textEditor.document.getText());
-			this.updateMonsterLife(this.monsterLife);
+      if (this.monsterLife === 0) {
+        this.changeMonster();
+        this.resetMonsterLife();
+      }
+    });
+  }
 
-			if (this.monsterLife === 0) {
-				this.changeMonster();
-				this.resetMonsterLife();
-			}
-		});
-	}
+  private updateMonsterStyle() {
+    const webview = this._view?.webview;
 
-	private updateMonsterStyle(text: string) {
-		const webview = this._view?.webview;
+    if (webview) {
+      webview.postMessage({
+        command: "updateMonsterStyle",
+      });
+    }
+  }
 
-		if (webview) {
-				webview.postMessage({ command: 'updateMonsterStyle', value: text.length });
-		}
-	}
+  private updateMonsterLife(lifePoint: number) {
+    const webview = this._view?.webview;
 
-	private updateMonsterLife(lifePoint: number) {
-		const webview = this._view?.webview;
+    if (webview) {
+      webview.postMessage({ command: "updateMonsterLife", value: lifePoint });
+    }
+  }
 
-		if (webview) {
-				webview.postMessage({ command: 'updateMonsterLife', value: lifePoint });
-		}
-	}
+  private changeMonster() {
+    const webview = this._view?.webview;
 
-	private changeMonster() {
-			const webview = this._view?.webview;
-			let anotherMonster = this.listMonster.filter(item => item !== this.defaultMonster);
-			let random = Math.floor(Math.random() * anotherMonster.length);
-			console.log(anotherMonster);
-			this.defaultMonster = anotherMonster[random];
+    this.range1to10 = this.getRandomInt(1, 10);
+    this.monsterView = this.monsterImg + this.range1to10.toString() + ".png";
 
-			if (webview) {
-				const imageUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', this.defaultMonster));
-	
-					webview.postMessage({ command: 'changeMonster', value: imageUri.toString() });
-			}
-	}
+    if (webview) {
+      const imageUri = webview.asWebviewUri(
+        vscode.Uri.joinPath(
+          this._extensionUri,
+          "media",
+          "monster",
+          this.monsterView
+        )
+      );
 
-	private resetMonsterLife() {
-		this.monsterLife = 30;
-		this.updateMonsterLife(this.monsterLife);
-	}
+      webview.postMessage({
+        command: "changeMonster",
+        value: imageUri.toString(),
+      });
+    }
+  }
 
-	private _getHtmlForWebview(webview: vscode.Webview) {
-		const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'main.js'));
-		const styleMainUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'main.css'));
-		const styleHealthUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'healthbar.css'));
-		const imageMonsterUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'Frog.png'));
+  private resetMonsterLife() {
+    this.monsterLife = 30;
+    this.updateMonsterLife(this.monsterLife);
+  }
 
-		return `<!DOCTYPE html>
+  private getRandomInt(min: number, max: number): number {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  private _getHtmlForWebview(webview: vscode.Webview) {
+    const scriptUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "media", "main.js")
+    );
+    const styleMainUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "media", "main.css")
+    );
+    const styleHealthUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "media", "healthbar.css")
+    );
+    const imageMonsterUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(
+        this._extensionUri,
+        "media",
+        "monster",
+        this.monsterView
+      )
+    );
+
+    return `<!DOCTYPE html>
 			<html lang="en">
 			<head>
 				<meta charset="UTF-8">
             <meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src ${webview.cspSource}; style-src ${webview.cspSource}; script-src ${webview.cspSource};">
 				<link href="${styleMainUri}" rel="stylesheet">
 				<link href="${styleHealthUri}" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 				<title>RPG</title>
 			</head>
 			<body id="body">
-
+      <button class="tooltip">?
+      <span class="tooltiptext">
+        Monster by: <a href="https://craftpix.net">Craftpix</a>
+      </span>
+      </button>
 				<div class="box">
 
 					<div style="display: block;">
-
 						<div style="display: flex; justify-content: center; width: 100%;">
-						<img id="monster" class="monster" src="${imageMonsterUri}" alt="Monster" />
+						<img id="monster" class="monster pulse" src="${imageMonsterUri}" alt="Monster" />
 						</div>
 					</div>
 
@@ -123,5 +158,5 @@ class rpgViewProvider implements vscode.WebviewViewProvider {
 				<script src="${scriptUri}"></script>
 			</body>
 			</html>`;
-	}
+  }
 }
